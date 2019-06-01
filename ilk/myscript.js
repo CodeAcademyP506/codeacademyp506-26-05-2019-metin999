4905725af2e49772ec0bab967878783fdafb787e
metin999 <50264758+metin999@users.noreply.github.com>
2019-06-01 04:19:34 -0700

    Add files via upload

---
+++ ilk/myscript.js
@@ -0,0 +1,58 @@
+//var num = 12343;
+//var count = 0;
+//if (num >= 0) {
+//    while(num > 0) {
+//        qaliq = num % 10;
+//        num = (num-qaliq) / 10;
+//        count++;
+//    }
+//    console.log(count);
+//}else{
+//    console.log("match finished")
+//}
+
+
+//ex.1^
+
+
+//var max=0;
+//var arr = [5,7,4,8,6];
+//for(var i=0;i < arr.length;i++){
+//    if(arr[i] > max){
+//        max = arr[i];
+//    }
+//}
+//console.log(max);
+
+
+//ex.3^
+
+
+//var arr = [5,7,4,8,6];
+//var min=arr[0];
+//for(var i =0 ;i < arr.length;i++){
+//    if(arr[i] < min){
+//        min = arr[i];
+//    }
+//}
+//console.log(min);
+
+//ex.4^
+
+
+//var result=1;
+//for(count = 1;count <=5;count++){
+//    result=result * count;
+//    {
+//    console.log(result);
+//    }
+//    
+//}
+
+
+//ex.5^
+
+
+
+
+
